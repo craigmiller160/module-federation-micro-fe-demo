a35d750fd27a5dc84d2b14b35221548c7b777280
Craig Miller <craigmiller160@gmail.com>
2021-08-16 14:13:11 -0400

    Refactored styles in react child to use common variables and mixins.

--- applications/svelte-child-1/src/components/UserDetails.svelte
+++ applications/svelte-child-1/src/components/UserDetails.svelte
@@ -1,1 +1,25 @@
-<h3>User Details</h3>
+<style lang="scss">
+  .UserDetails {
+    margin: 1rem;
+    padding: 2rem;
+    border: 1px solid darkgreen;
+    border-radius: 5px;
+    font-size: 1.5rem;
+
+    :global(textarea) {
+      width: 100%;
+      height: 10rem;
+      resize: none;
+    }
+  }
+</style>
+
+<script>
+
+</script>
+
+<div class="UserDetails">
+    <p>
+        <strong>Name: </strong>
+    </p>
+</div>
