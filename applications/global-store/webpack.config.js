be630f3cc832d474cecd6b5314cdae6b9c749b4c
Craig Miller <craigmiller160@gmail.com>
2021-08-16 11:09:28 -0400

    Setup module federation in global store

--- applications/global-store/webpack.config.js
+++ applications/global-store/webpack.config.js
@@ -1,11 +1,21 @@
 const baseConfig = require('@mfdemo/webpack-base/webpack.config');
 const { merge } = require('webpack-merge');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 
 module.exports = merge(
     baseConfig,
     {
         devServer: {
             port: 3001
-        }
+        },
+        plugins: [
+            new ModuleFederationPlugin({
+                name: 'globalStore',
+                filename: 'remoteEntry.js',
+                exposes: {
+                    '.': './src/index.js'
+                }
+            })
+        ]
     }
 )
