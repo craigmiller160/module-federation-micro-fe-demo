e2ee0f364dd2c3616fe37cac85fdf00eac461973
Craig Miller <craigmiller160@gmail.com>
2021-08-16 11:49:06 -0400

    Updated react api:

--- utilities/create-web-component-react/src/index.js
+++ utilities/create-web-component-react/src/index.js
@@ -3,8 +3,8 @@ import { createWebComponent } from '@mfdemo/create-web-component';
 
 export const createWebComponentReact = (name, ReactComp) => {
     const WebComponent = createWebComponent({
-        connectMicroFrontend: (root) => ReactDOM.render(<ReactComp />, root),
-        disconnectMicroFrontend: (root) => ReactDOM.unmountComponentAtNode(root)
+        connectComponent: (root) => ReactDOM.render(<ReactComp />, root),
+        disconnectComponent: (root) => ReactDOM.unmountComponentAtNode(root)
     });
     customElements.define(name, WebComponent);
 };
