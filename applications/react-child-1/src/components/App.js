5435ca0943d94d457f1ee0834812abe0fdce8368
Craig Miller <craigmiller160@gmail.com>
2021-08-16 09:21:47 -0400

    Making progress building application

--- applications/react-child-1/src/components/App.js
+++ applications/react-child-1/src/components/App.js
@@ -1,10 +1,18 @@
 import React from 'react'; // TODO try getting rid of this
 import classes from './App.module.scss';
+import { UserList } from './UserList';
+import { UserDetails } from './UserDetails';
 
-export const App = () => (
-    <div className={ classes.App }>
-        <div className={ classes.title }>
-            <h1>React Child 1</h1>
+export const App = () => {
+    return (
+        <div className={ classes.App }>
+            <div className={ classes.title }>
+                <h1>React Child 1</h1>
+            </div>
+            <div className={ classes.content }>
+                <UserList />
+                <UserDetails />
+            </div>
         </div>
-    </div>
-);
+    );
+};
--- applications/react-child-1/src/components/UserList.js
+++ applications/react-child-1/src/components/UserList.js
@@ -1,1 +1,16 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import axios from 'axios';
+
+export const UserList = () => {
+    useEffect(() => {
+        const request = async () => {
+            const res = await axios.get('https://reqres.in/api/users');
+            console.log(res.data);
+        };
+
+    }, []);
+
+    return (
+        <h3>User List</h3>
+    );
+};
