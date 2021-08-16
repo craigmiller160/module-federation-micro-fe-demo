063c39148f995dbe7d4e1bb99045dbf9e77b9558
Craig Miller <craigmiller160@gmail.com>
2021-08-16 11:57:26 -0400

    Got core of svelte micro-frontend setup

--- applications/svelte-child-1/src/bootstrap.js
+++ applications/svelte-child-1/src/bootstrap.js
@@ -1,2 +1,6 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
+import { createWebComponentSvelte } from '@mfdemo/create-web-component-svelte';
+import App from './components/App.svelte';
+
+createWebComponentSvelte('svelte-child-1', App);
