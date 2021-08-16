9f7396c8168ed47312df60e6d488319386e1ff56
Craig Miller <craigmiller160@gmail.com>
2021-08-16 09:09:01 -0400

    Committing progress

--- applications/react-child-1/src/components/App.js
+++ applications/react-child-1/src/components/App.js
@@ -1,5 +1,10 @@
 import React from 'react'; // TODO try getting rid of this
+import classes from './App.module.scss';
 
 export const App = () => (
-    <h1>Hello World</h1>
+    <div className={ classes.App }>
+        <div className={ classes.title }>
+            <h1>React Child 1</h1>
+        </div>
+    </div>
 );
