2d84ea3b509c730b4cf9f30fdcb4d79b15fb1554
Craig Miller <craigmiller160@gmail.com>
2021-08-16 09:23:08 -0400

    Added polyfills.

--- applications/react-child-1/src/bootstrap.js
+++ applications/react-child-1/src/bootstrap.js
@@ -1,3 +1,5 @@
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
 import { App } from './components/App';
 import { createWebComponentReact } from '@mfdemo/create-web-component-react';
 
