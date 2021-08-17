f29db538303cead09370765cadef03b94b66483b
Craig Miller <craigmiller160@gmail.com>
2021-08-17 17:43:22 -0400

    Working on changing how svelte project is exported

--- utilities/global-router-svelte-navigator/webpack.config.js
+++ utilities/global-router-svelte-navigator/webpack.config.js
@@ -1,10 +1,15 @@
 const baseConfig = require('@mfdemo/webpack-base/webpack.config');
 const { merge } = require('webpack-merge');
 const preprocess = require('svelte-preprocess');
+const path = require('path');
 
 module.exports = merge(
     baseConfig,
     {
+        entry: {
+            index: path.join(__dirname, 'src', 'index.js'),
+            vendor: ['svelte', 'svelte-navigator']
+        },
         module: {
             rules: [
                 {
