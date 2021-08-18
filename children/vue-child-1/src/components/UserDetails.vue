e319130febb9e36eca5f01d1ab835a4eb521ce91
Craig Miller <craigmiller160@gmail.com>
2021-08-18 17:46:36 -0400

    Committing progress

--- children/vue-child-1/src/components/UserDetails.vue
+++ children/vue-child-1/src/components/UserDetails.vue
@@ -1,18 +1,60 @@
 <template>
-
+  <div class="UserDetails">
+    <p>
+      <strong>Name: </strong>
+      {{ selectedUser.first_name }} {{ selectedUser.last_name }}
+    </p>
+  </div>
 </template>
 
 <script>
+import { useSelectedUser } from './useSelectedUser';
+import { onMounted, onUnmounted, ref } from 'vue';
+import { subscribe, updateState } from 'globalStore';
+
 export default {
   props: [
       'users'
   ],
   setup(props) {
+    const selectedUser = useSelectedUser(props.users);
+    const notes = ref('');
 
+    let storeUnsubscribe;
+
+    onMounted(() => {
+      storeUnsubscribe = subscribe((state) => {
+        notes.value = state.userNotes?.[selectedUser?.value?.id] ?? '';
+      });
+    });
+
+    onUnmounted(() => {
+      if (storeUnsubscribe) {
+        storeUnsubscribe();
+      }
+    });
+
+    return {
+      selectedUser
+    }
   }
 }
 </script>
 
 <style scoped lang="scss">
+  @import "src/styles/common";
 
+  .UserDetails {
+    margin: 1rem;
+    padding: 2rem;
+    font-size: $text-size-content;
+
+    @include border-light;
+
+    textarea {
+      width: 100%;
+      height: 10rem;
+      resize: none;
+    }
+  }
 </style>
