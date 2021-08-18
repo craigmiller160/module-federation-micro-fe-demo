f9846e155aa6c6bcb94dbb1328bb2ea8dd8a4a71
Craig Miller <craigmiller160@gmail.com>
2021-08-17 21:19:40 -0400

    Setup global router in react

--- children/react-child-1/src/components/App.js
+++ children/react-child-1/src/components/App.js
@@ -1,8 +1,10 @@
 import { Content } from './Content';
 import { BrowserRouter } from 'react-router-dom';
+import { GlobalRouterConnector } from '@mfdemo/global-router-react-router';
 
 export const App = () => (
     <BrowserRouter>
+        <GlobalRouterConnector />
         <Content />
     </BrowserRouter>
 );
