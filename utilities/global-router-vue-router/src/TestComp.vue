d7bff0acbe9425f8da30251481a20fe2ad5f14b5
Craig Miller <craigmiller160@gmail.com>
2021-08-19 15:48:26 -0400

    Added test component

---
+++ utilities/global-router-vue-router/src/TestComp.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>Hello World</h1>
+</template>
+
+<script>
+export default {
+  name: "TestComp",
+  setup() {
+    console.log('FooBar');
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
