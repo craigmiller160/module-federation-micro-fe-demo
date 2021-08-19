850a9d76715d1ca8cf36732677d6ec371d2e5750
Craig Miller <craigmiller160@gmail.com>
2021-08-19 16:19:07 -0400

    Integrated GlobalRouterConnector

--- children/vue-child-1/src/components/App.vue
+++ children/vue-child-1/src/components/App.vue
@@ -1,12 +1,14 @@
 <template>
+  <GlobalRouterConnector />
   <Content />
 </template>
 
 <script>
 import Content from './Content';
+import { GlobalRouterConnector } from '@mfdemo/global-router-vue-router';
 export default {
   name: "App",
-  components: { Content }
+  components: { Content, GlobalRouterConnector }
 }
 </script>
 
