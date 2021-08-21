faa4c76ac4911736cdaead05b2b1ecc18eaed6d2
Craig Miller <craigmiller160@gmail.com>
2021-08-21 15:46:09 -0400

    Making big progress with upgrade script

--- upgrade.sh
+++ upgrade.sh
@@ -17,18 +17,24 @@ doUpgrade() {
     fullPath="$currentDir/$1/$dirName"
     dependencyMatch=$(cat "$fullPath/package.json" | grep "$2")
     if [[ "$dependencyMatch" != "" ]]; then
-      echo "Upgrading $dirName"
+      echo "Upgrading $2 in $dirName"
       cd $fullPath
       errors=$(yarn upgrade $2 2>&1 >/dev/null)
       printErrors "$errors"
     else
-      echo "Not upgrading $dirName"
+      echo "Not upgrading $2 in $dirName"
     fi
   done
 }
+
+if [[ $# -ne 1 ]]; then
+  echo "Must provide name of dependency to upgrade"
+  exit 1
+fi
 
 doUpgrade 'parents' "$1"
 cd $currentDir
 doUpgrade 'children' "$1"
 cd $currentDir
 doUpgrade 'utilities' "$1"
+exit 0
