6d71e98712e93cb6b3b0fd59a8bde38973536bd3
Craig <craigmiller160@gmail.com>
2021-09-05 15:34:34 -0400

    Making progress with server

---
+++ server/src/createRoute.js
@@ -0,0 +1,36 @@
+const {
+    getCacheControl,
+    getFilePath,
+    getContentType
+} = require('./utilities');
+const fs = require('fs');
+const path = require('path');
+
+const createRoute = (app, buildRoot) => {
+    app.get('/**', (req, res) => {
+        console.log(`Received request: ${req.method} ${req.path}`);
+        const filePath = getFilePath(buildRoot, req.path);
+        if (!fs.existsSync(filePath)) {
+            console.error(`Cannot find file: ${filePath}`);
+            res.status(404);
+            res.set({
+                'Content-Type': 'text/plain'
+            })
+            res.send(`Cannot find file: ${filePath}`);
+            return;
+        }
+
+        console.log(`Returning file: ${filePath}`);
+
+        const extension = path.extname(filePath);
+        const content = fs.readFileSync(filePath, 'utf8');
+
+        res.set({
+            'Content-Type': getContentType(extension),
+            'Cache-Control': getCacheControl(req.path)
+        })
+        res.send(content);
+    });
+};
+
+module.exports = createRoute;
