00880bb91ebe2b7da9f506331694f2d0d0592113
Craig <craigmiller160@gmail.com>
2021-09-06 10:44:10 -0400

    Setup react child 2

---
+++ children/react-child-2/webpack.config.js
@@ -0,0 +1,56 @@
+const baseConfig = require('@mfdemo/webpack-base/webpack.config');
+const { merge } = require('webpack-merge');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+
+const { dependencies } = require('./package.json');
+const shareDeps = {};
+if (process.env.SHARE_DEPS === 'true') {
+    shareDeps.shared = {
+        ...(dependencies || {}),
+        '@mfdemo/create-web-component': {
+            singleton: true
+        }
+    };
+}
+
+module.exports = merge(
+    baseConfig,
+    {
+        devServer: {
+            port: 3002,
+            proxy: {
+                '/globalStore': {
+                    target: 'http://localhost:3001',
+                    changeOrigin: true,
+                    pathRewrite: {
+                        '^/globalStore': ''
+                    }
+                }
+            }
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.jsx?$/,
+                    exclude: /node_modules/,
+                    use: [
+                        'babel-loader'
+                    ]
+                }
+            ]
+        },
+        plugins: [
+            new ModuleFederationPlugin({
+                name: 'reactChild1',
+                filename: 'remoteEntry.js',
+                remotes: {
+                    globalStore: 'globalStore@/globalStore/remoteEntry.js'
+                },
+                exposes: {
+                    '.': './src/bootstrap.js'
+                },
+                ...shareDeps
+            })
+        ]
+    }
+);
