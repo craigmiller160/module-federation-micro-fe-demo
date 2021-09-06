5c41eb4324742ce6ca3358d96d2a08c01943add7
Craig <craigmiller160@gmail.com>
2021-09-06 11:09:23 -0400

    Changed web comp name

--- children/react-child-2/src/bootstrap.js
+++ children/react-child-2/src/bootstrap.js
@@ -1,4 +1,4 @@
 import { App } from './components/App';
 import { createWebComponentReact } from '@mfdemo/create-web-component-react';
 
-createWebComponentReact('react-child-1', App);
+createWebComponentReact('react-child-2', App);
