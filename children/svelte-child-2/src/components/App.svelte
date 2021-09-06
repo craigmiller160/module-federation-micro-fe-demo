5c3046754a46d39fd7cbfa730b9efc754dd1fdde
Craig <craigmiller160@gmail.com>
2021-09-06 11:53:52 -0400

    Trying to make svelte micro-frontends work

--- children/svelte-child-2/src/components/App.svelte
+++ children/svelte-child-2/src/components/App.svelte
@@ -1,7 +1,7 @@
 <script>
     import { Router } from 'svelte-navigator';
     import Content from './Content.svelte';
-    import { GlobalRouterConnector } from '@mfdemo/global-router-svelte-navigator';
+    import { GlobalRouterConnector } from '@mfdemo/global-router-svelte-navigator/src';
 </script>
 
 <Router>
