e1fec4d7d6ce7358e7460da4953dd6a74fe4ddbc
Craig <craigmiller160@gmail.com>
2021-09-06 09:33:29 -0400

    Setup share behavior for vanilla-parent

--- parents/vanilla-parent/webpack.config.js
+++ parents/vanilla-parent/webpack.config.js
@@ -2,6 +2,14 @@ const baseConfig = require('@mfdemo/webpack-base/webpack.config');
 const { merge } = require('webpack-merge');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 
+const { dependencies } = require('./package.json');
+const shareDeps = {};
+if (process.env.SHARE_DEPS === 'true') {
+    shareDeps.shared = {
+        ...(dependencies || {})
+    };
+}
+
 module.exports = merge(
     baseConfig,
     {
@@ -46,7 +54,8 @@ module.exports = merge(
                     reactChild1: 'reactChild1@/reactChild1/remoteEntry.js',
                     svelteChild1: 'svelteChild1@/svelteChild1/remoteEntry.js',
                     vueChild1: 'vueChild1@/vueChild1/remoteEntry.js'
-                }
+                },
+                ...shareDeps
             })
         ]
     }
