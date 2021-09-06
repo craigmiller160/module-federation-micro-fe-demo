9f4a4e2c7a43e82a818f9bd34b849f0e83577b42
Craig <craigmiller160@gmail.com>
2021-09-06 11:47:14 -0400

    Changed web comp name

--- children/vue-child-2/src/bootstrap.js
+++ children/vue-child-2/src/bootstrap.js
@@ -6,4 +6,4 @@ import { createWebComponentVue } from '@mfdemo/create-web-component-vue';
 const app = createApp(App)
     .use(router);
 
-createWebComponentVue('vue-child-1', app);
+createWebComponentVue('vue-child-2', app);
