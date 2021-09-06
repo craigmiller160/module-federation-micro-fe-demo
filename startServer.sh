0cc79b8a92ff2c97eb996737d00b470c37c1f599
Craig <craigmiller160@gmail.com>
2021-09-06 11:03:01 -0400

    More start/stop script work

--- startServer.sh
+++ startServer.sh
@@ -3,5 +3,10 @@
 rootDir=$(pwd)
 
 cd server
-yarn start:vanillaParent & # 1>/dev/null 2>/dev/null
+yarn start:vanillaParent & 1>/dev/null 2>/dev/null
+yarn start:reactChild1 & 1>/dev/null 2>/dev/null
+yarn start:globalStore & 1>/dev/null 2>/dev/null
+yarn start:vueChild1 & 1>/dev/null 2>/dev/null
+yarn start:svelteChild1 & 1>/dev/null 2>/dev/null
+yarn start:reactChild2 & 1>/dev/null 2>/dev/null
 
--- stopServer.sh
+++ stopServer.sh
@@ -2,3 +2,15 @@
 
 vanillaParentPid=$(sudo lsof -t -i:3000)
 sudo kill $vanillaParentPid
+
+globalStorePid=$(sudo lsof -t -i:3001)
+sudo kill $globalStorePid
+
+reactChild1Pid=$(sudo lsof -t -i:3002)
+sudo kill $reactChild1Pid
+
+vueChild1Pid=$(sudo lsof -t -i:3004)
+sudo kill $vueChild1Pid
+
+svelteChild1Pid=$(sudo lsof -t -i:3003)
+sudo kill $svelteChild1Pid
