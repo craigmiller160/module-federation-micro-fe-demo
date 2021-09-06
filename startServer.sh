3347b9cac04504689568ea0bd6735b0df7491f40
Craig <craigmiller160@gmail.com>
2021-09-06 11:25:19 -0400

    Enhanced start/stop scripts

--- startServer.sh
+++ startServer.sh
@@ -3,10 +3,18 @@
 rootDir=$(pwd)
 
 cd server
-yarn start:vanillaParent & 1>/dev/null 2>/dev/null
-yarn start:reactChild1 & 1>/dev/null 2>/dev/null
-yarn start:globalStore & 1>/dev/null 2>/dev/null
-yarn start:vueChild1 & 1>/dev/null 2>/dev/null
-yarn start:svelteChild1 & 1>/dev/null 2>/dev/null
-yarn start:reactChild2 & 1>/dev/null 2>/dev/null
+echo "Starting Vanilla Parent"
+yarn start:vanillaParent 1>/dev/null 2>/dev/null &
+echo "Starting Global Store"
+yarn start:globalStore 1>/dev/null 2>/dev/null &
+echo "Starting React Child 1"
+yarn start:reactChild1 1>/dev/null 2>/dev/null &
+echo "Starting Vue Child 1"
+yarn start:vueChild1 1>/dev/null 2>/dev/null &
+echo "Starting Svelte Child 1"
+yarn start:svelteChild1 1>/dev/null 2>/dev/null &
+echo "Starting React Child 2"
+yarn start:reactChild2 1>/dev/null 2>/dev/null &
+echo "Starting Svelte Child 2"
+yarn start:svelteChild2 1>/dev/null 2>/dev/null &
 
--- stopServer.sh
+++ stopServer.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
 
+echo "Killing Vanilla Parent"
 vanillaParentPid=$(sudo lsof -t -i:3000)
 sudo kill $vanillaParentPid
 
+echo "Killing Global Store"
 globalStorePid=$(sudo lsof -t -i:3001)
 sudo kill $globalStorePid
 
+echo "Killing React Child 1"
 reactChild1Pid=$(sudo lsof -t -i:3002)
 sudo kill $reactChild1Pid
 
+echo "Killing Vue Child 1"
 vueChild1Pid=$(sudo lsof -t -i:3004)
 sudo kill $vueChild1Pid
 
+echo "Killing Svelte Child 1"
 svelteChild1Pid=$(sudo lsof -t -i:3003)
 sudo kill $svelteChild1Pid
 
+echo "Killing React Child 2"
 reactChild2Pid=$(sudo lsof -t -i:3005)
 sudo kill $reactChild2Pid
+
+echo "Killing Svelte Child 2"
+svelteChild2Pid=$(sudo lsof -t -i:3006)
+sudo kill $svelteChild2Pid
