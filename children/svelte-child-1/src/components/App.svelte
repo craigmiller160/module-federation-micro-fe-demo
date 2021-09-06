c8e4845b03948f606cdb8658feaeb31706d3ef32
Craig <craigmiller160@gmail.com>
2021-09-06 12:06:13 -0400

    Trying even more things

--- children/svelte-child-1/src/components/App.svelte
+++ children/svelte-child-1/src/components/App.svelte
@@ -2,9 +2,11 @@
     import { Router } from 'svelte-navigator';
     import Content from './Content.svelte';
     import { GlobalRouterConnector } from '@mfdemo/global-router-svelte-navigator/src';
+
+    // TODO primary=false might not be necessary
 </script>
 
-<Router>
+<Router primary={ false }>
     <GlobalRouterConnector />
     <Content />
 </Router>
